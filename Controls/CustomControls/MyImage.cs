d271cccae241db6ddcf07a1bcba448a32e1c1937
lucnv <lucnv@bagroup.vn>
2024-10-28 17:12:05 +0700

    custom màu của ảnh

---
+++ Controls/CustomControls/MyImage.cs
@@ -0,0 +1,22 @@
+﻿using CommunityToolkit.Maui.Behaviors;
+
+namespace TutorialMaUI.Controls.CustomControls
+{
+    public class MyImage : Image
+    {
+        public Color TintColor
+        {
+            get { return (Color)GetValue(TintColorProperty); }
+            set { SetValue(TintColorProperty, value); }
+        }
+
+        public static readonly BindableProperty TintColorProperty = BindableProperty.Create(
+        nameof(TintColor), typeof(Color), typeof(MyImage), null, BindingMode.TwoWay, propertyChanged: OnTintColorPropertyChanged);
+
+        private static void OnTintColorPropertyChanged(BindableObject bindable, object oldValue, object newValue)
+        {
+            var control = (MyImage)bindable;
+            control.Behaviors.Add(new IconTintColorBehavior { TintColor = (Color)newValue });
+        }
+    }
+}
